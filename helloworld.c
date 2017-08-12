8de8f62e32fcd1e6ac297a6e516dfefa065f78c2
Philip Sokolov <511sokolov@gmail.com>
2017-08-12 14:33:34 +0200

    changed an error (\c)

--- helloworld.c
+++ helloworld.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 //main
-int main()                    //Define the main function called main. That receives no arguements.
-{                             //The statements of main, or it's body are enclosed in {}
-    printf("hello, \c world\n"); //Print helloworld to the terminal. Main calls <stdio.h> function printf, \n represents a newline.
+int main()                      //Define the main function called main. That receives no arguements.
+{                               //The statements of main, or it's body are enclosed in {}
+    printf("hello,  world!\n"); //Print helloworld to the terminal. Main calls <stdio.h> function printf, \n represents a newline.
 }
